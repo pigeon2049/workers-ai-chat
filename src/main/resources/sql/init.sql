0dcc1c5ae77344d5d4f66d14624d85b4c52c8e43
zhanguilai <zhanguilai@gmail.com>
2024-04-18 15:32:53 +0800

    update init.sql

--- src/main/resources/sql/init.sql
+++ src/main/resources/sql/init.sql
@@ -1,9 +1,9 @@
 CREATE SEQUENCE IF NOT EXISTS chat_id_seq START 1;
 CREATE TABLE IF NOT EXISTS chat_record (
     id BIGINT PRIMARY KEY DEFAULT NEXTVAL('chat_id_seq'),
-    role VARCHAR(10),
+    role VARCHAR,
     content TEXT,
     time TIMESTAMP,
     user_id BIGINT,
-    c_id VARCHAR(64)
+    c_id VARCHAR
     );
